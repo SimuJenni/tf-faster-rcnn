02b21d81d0e01d053a54bef69cc450cb54860184
SimuJenni <simujenni@gmail.com>
2017-03-10 11:16:30 +0100

    test rcnn

--- lib/nets/toonnet.py
+++ lib/nets/toonnet.py
@@ -46,8 +46,8 @@ def toon_net_argscope(activation=tf.nn.relu, kernel_size=(3, 3), padding='SAME',
     """
     batch_norm_params = {
         'is_training': False,
-        'decay': 0.995,
-        'epsilon': 1e-5,
+        'decay': 0.95,
+        'epsilon': 0.001,
         'center': center,
         # 'trainable': False
     }
@@ -117,7 +117,7 @@ class ToonNet(Network):
                 biases_regularizer = None
             else:
                 biases_regularizer = tf.no_regularizer
-            rpn = slim.conv2d(net, 256, [3, 3], trainable=is_training, weights_initializer=initializer,
+            rpn = slim.conv2d(net, 512, [3, 3], trainable=is_training, weights_initializer=initializer,
                               biases_regularizer=biases_regularizer,
                               biases_initializer=tf.constant_initializer(0.0), scope="rpn_conv/3x3")
             self._act_summaries.append(rpn)
